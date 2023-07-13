a48c9c4c391da203fe879df13a5ad8b378a4abc6
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2023-07-13 23:53:00 +0600

    Update script.js

--- site3/script.js
+++ site3/script.js
@@ -1,20 +1,20 @@
-function getRandomNumber(min, max) {
-  var seed = new Date().getTime();
-  var random = function() {
-    seed = (seed * 9301 + 49297) % 233280;
-    return seed / 233280;
-  };
-  return Math.floor(random() * (max - min + 1)) + min;
-}
 
-// Example usage: Generating a random number between 1 and 10
-var correct = getRandomNumber(1, 10);
-console.log(correct);
-
-
+function game(){
+  function getRandomNumber(min, max) {
+    var seed = new Date().getTime();
+    var random = function() {
+      seed = (seed * 9301 + 49297) % 233280;
+      return seed / 233280;
+    };
+    return Math.floor(random() * (max - min + 1)) + min;
+  }
+  
+  var correct = getRandomNumber(1, 10);
 
   for (var i = 0; i < 3; i++) {
+  alert("You have left " + (3-i) + " chances")
   var userinput = prompt('Enter a random number between 1 and 10');
+
   console.log(userinput);
   if (userinput==correct) {
     alert('You Win!');
@@ -35,4 +35,10 @@ console.log(correct);
   {
     alert('You Lose!');
   }
+  var s = prompt("If you want to start again type start")
+  if(s=="start"){
+    game();
+  }
+}
 
+game();
